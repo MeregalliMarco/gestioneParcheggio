6d328f4fc6dd6f631095467091e56344270e3b6f
MeregalliMarco <105349339+MeregalliMarco@users.noreply.github.com>
2022-05-13 10:51:14 +0200

    Add files via upload

---
+++ FXMLDocumentController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestioneparcheggio;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import static javafx.scene.paint.Color.*;
+
+/**
+ *
+ * @author meregalli.marco
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label dispLabel;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+        dispLabel.setTextFill(GREEN);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
